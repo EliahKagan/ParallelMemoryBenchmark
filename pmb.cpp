48c709a714f5b4827da1f1170f3b965523ad1dcd
Eliah Kagan <degeneracypressure@gmail.com>
2018-08-02 14:26:59 -0400

    Copyedit comment

--- pmb.cpp
+++ pmb.cpp
@@ -85,8 +85,8 @@ namespace fmt {
 
 namespace {
     // TODO: Consider making a template that itself binds a function template as
-    //       a template parameter, for run-time selction of execution policy for
-    //       any algorithm that supports it. The algorithm would still be
+    //       a template parameter, for run-time selection of execution policy
+    //       for any algorithm that supports them. The algorithm would still be
     //       selected at compile-time, but the execution policy at run-time.
     template<typename RandomIt>
     void
