864070a82435cbc9c01a6e968f8de628d6d74ab6
Eliah Kagan <degeneracypressure@gmail.com>
2018-08-01 21:31:36 -0400

    Decisecond precision

--- pmb.cpp
+++ pmb.cpp
@@ -373,6 +373,6 @@ int main(int argc, char** argv)
     const auto dt = tf - ti;
 
     // TODO: consider printing seconds with tenths precision
-    fmt::print("\nTest completed in about {:.0f} s ({} ms).\n",
+    fmt::print("\nTest completed in about {:.1f} s ({} ms).\n",
                dt / 1.0s, dt / 1ms);
 }
